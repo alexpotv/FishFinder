f886e4febe61f414a638eeffa85356090140bcf4
Alexandre Potvin-Demers <alexpotvind@icloud.com>
2022-06-26 15:50:29 -0400

    Refactoring for frame fetcher usage elsewhere in the project, added sample data collected while testing.

--- utils/frame_fetcher/data_collection.py
+++ utils/frame_fetcher/data_collection.py
@@ -4,7 +4,11 @@ Frame fetcher tool for recuperating frames from a web-based livestream
 import os
 import datetime
 import configparser
+from time import sleep
+
 from colorama import Fore
+from frame_fetcher import get_frame
+import cv2
 
 
 def print_log(msg_type: str, message: str):
@@ -25,27 +29,33 @@ def print_log(msg_type: str, message: str):
 
     print(f"{color}{datetime.datetime.now().strftime('%H:%M:%S')} - [{msg_type}] {message}")
 
+
+CONFIG_PATH = "./utils/frame_fetcher/FETCHER_CONFIG.ini"
+
 print_log("LOG", "Reading configuration file...")
 config = configparser.ConfigParser()
-config.read("./tools/frame_fetcher/FETCHER_CONFIG.ini")
+config.read(CONFIG_PATH)
 print_log("OK", "Configuration file read successfully!")
 
 START_TIME = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 YOUTUBE_URL = config['DEFAULT']['YOUTUBE_URL']
 TIME_DELAY = config['DEFAULT']['TIME_DELAY']
 TOTAL_DURATION = config['DEFAULT']['TOTAL_DURATION']
-
-print_log("LOG", "Creating direct live-stream URL for resource...")
-FETCH_URL = os.popen(f"youtube-dl -g {YOUTUBE_URL}").read()
-print_log("OK", "Live stream direct URL successfully created!")
+nb_iterations = int(float(TOTAL_DURATION)/float(TIME_DELAY))
 
 print_log("LOG", f"Creating directory for {START_TIME} frame fetch run...")
 os.mkdir(f"./model/data/raw_data/{START_TIME}")
 print_log("OK", "Directory created successfully!")
 
 print_log("LOG", f"Launching frame fetch run for {TOTAL_DURATION} seconds...")
-os.system(f"ffmpeg -hide_banner -loglevel error -i \"{FETCH_URL}\" -vf fps=1/{TIME_DELAY} -t {TOTAL_DURATION} "
-          f"model/data/raw_data/{START_TIME}/out%d.png")
+for i in range(1, nb_iterations+1):
+    current_frame = get_frame()
+    try:
+        cv2.imwrite(f"./model/data/raw_data/{START_TIME}/out{i}.png", current_frame)
+        print_log("OK", f"Fetched and saved frame {i}.")
+    except cv2.error:
+        print_log("WARN", f"Could not fetch frame number {i}, moving on to next iteration...")
+    sleep(float(TIME_DELAY))
 print_log("OK", f"Finished {START_TIME} frame fetch run!")
 
 print_log("WARN", "Remember to run \"dvc add\" to track newly added data files!")
---
+++ utils/frame_fetcher/frame_fetcher.py
@@ -0,0 +1,23 @@
+"""
+Python script for fetching the current frame from the live-stream
+"""
+import os
+import configparser
+import cv2
+
+
+def get_frame():
+    """
+    Fetches the current frame from the live-stream
+    :return: The frame, as an object
+    """
+    config = configparser.ConfigParser()
+    config.read("./utils/frame_fetcher/FETCHER_CONFIG.ini")
+    youtube_url = config['DEFAULT']['YOUTUBE_URL']
+
+    fetch_url = os.popen(f"youtube-dl -g {youtube_url}").read()
+
+    capture_object = cv2.VideoCapture(fetch_url)
+    _, frame = capture_object.read()
+
+    return frame
