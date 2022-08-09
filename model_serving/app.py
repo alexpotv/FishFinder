3cc0f521be3311ff44087866baa21788a9179a92
Alexandre Potvin-Demers <alexpotvind@icloud.com>
2022-08-09 17:28:56 -0400

    Receiving image file using PIL

--- model_serving/app.py
+++ model_serving/app.py
@@ -1,14 +1,48 @@
 """
 Flask app for serving a REST API and interacting with the model
 """
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, Response
+import numpy as np
+import cv2
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/health', methods=["GET"])
 def health_check():
     return jsonify(
         {
             "status": "ok"
         }
     )
+
+
+@app.route('/detect', methods=["POST"])
+def detect():
+    """
+    Performs the inference using the model on the provided image
+    :return: Response object
+    """
+    if len(request.files) > 1:
+        return Response(
+            {
+                "error": "MULTIPLE_IMAGE_FILES",
+                "description": "More than one image file was provided."
+            },
+            status=400
+        )
+    elif len(request.files) < 1:
+        return Response(
+            {
+                "error": "INVALID_IMAGE_FILE",
+                "description": "The provided image file is invalid."
+            },
+            status=400
+        )
+
+    print(request.files.get("image_file").stream.read())
+
+    im = Image.open(request.files.get("image_file").stream)
+    im.show()
