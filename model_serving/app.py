6acae8ae8379b8b31ac9178bc489f27ec513854f
Alexandre Potvin-Demers <alexpotvind@icloud.com>
2022-08-09 20:54:31 -0400

    Added model to serving directory, returning error codes for bad requests

--- model_serving/app.py
+++ model_serving/app.py
@@ -4,19 +4,34 @@ Flask app for serving a REST API and interacting with the model
 from flask import Flask, request, jsonify, Response
 import numpy as np
 import cv2
-from io import BytesIO
-from PIL import Image
+import torch
+import json
 
 app = Flask(__name__)
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='./model_serving/model', force_reload=True)
+
+
+def build_err_response(error: str, description: str, status_code: int):
+    """
+    Builds an JSON string to be returned following an error
+    :param error: The error identifier
+    :param description: A description of the error
+    :param status_code: The HTTP status code to return
+    :return: The built Response object
+    """
+    json_response = json.dumps({
+        "error": error,
+        "description": description
+    })
+    return Response(
+        json_response, status=status_code, mimetype="application/json"
+    )
 
 
 @app.route('/health', methods=["GET"])
 def health_check():
-    return jsonify(
-        {
-            "status": "ok"
-        }
-    )
+    return {"status": "ok"}
 
 
 @app.route('/detect', methods=["POST"])
@@ -26,23 +41,40 @@ def detect():
     :return: Response object
     """
     if len(request.files) > 1:
-        return Response(
-            {
-                "error": "MULTIPLE_IMAGE_FILES",
-                "description": "More than one image file was provided."
-            },
-            status=400
-        )
+        return build_err_response("MULTIPLE_IMAGE_FILES",
+                                  "More than one image file was provided.",
+                                  400)
     elif len(request.files) < 1:
+        return build_err_response("NO_IMAGE_FILE",
+                                  "No image file was provided.",
+                                  400)
+
+    try:
+        # Reading image from file-like buffer
+        img_stream = request.files.get("image_file").stream
+        img_stream.seek(0)
+        img_array = np.asarray(bytearray(img_stream.read()), dtype=np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+        # Performing inference and returning results
+        results = model(img)
+        classes = {}
+        for i in range(len(results.names)):
+            classes[i] = results.names[i]
+
+        predictions = results.pred[0]
+
+        json_response = {
+            "labels": classes,
+            "detections": []
+        }
         return Response(
-            {
-                "error": "INVALID_IMAGE_FILE",
-                "description": "The provided image file is invalid."
-            },
-            status=400
+            json.dumps(json_response),
+            status=200,
+            mimetype="application/json"
         )
 
-    print(request.files.get("image_file").stream.read())
-
-    im = Image.open(request.files.get("image_file").stream)
-    im.show()
+    except Exception as e:
+        return build_err_response("INVALID_IMAGE_FILE",
+                                  "The image file provided is invalid.",
+                                  400)
